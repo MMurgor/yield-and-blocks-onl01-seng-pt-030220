40250355e343c567dd10da85fdfa02b3a5020503
Romena Murgor <romenamurgor@gmail.com>
2020-03-12 21:07:17 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,3 @@
-#def hello_t (array) 
-# return to_enum(:each)
-#i = 0
- # while i < array.length 
-  #yield (array[i]) 
-  #i += 1
-#end
-#array
-#end
-
-
 def hello_t (array) 
 if block_given?
 	i = 0
